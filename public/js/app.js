c1fb02568c83332d937d7f3e2d600f375e6e3b2b
Tyler Hedegard <tchedegard@gmail.com>
2016-01-28 04:05:30 +0000

    added body parser

--- public/js/app.js
+++ public/js/app.js
@@ -6,10 +6,11 @@
 
 $(document).ready(function() {
     var teamCount = 8;
+    var teamList = [];
     
     var getTeams = function() {
         var count = $(".teams").length;
-        var teamList = [];
+        teamList = []; //reset team list
         for (var i=1; i <= count; i+=1) {
             var team = "#team"+i;
             teamList.push($(team).val());
@@ -19,15 +20,12 @@ $(document).ready(function() {
     var getTeamCount = function() {
         teamCount = $(this).val();
         var domCount = $(".teams").length;
-        //console.log(teamCount);
         if (domCount < teamCount) {
             var delta =  teamCount - domCount;
-            console.log(delta);
             for (var i=1; i<=delta; i+=1) {
                 var newTeamNum = domCount+i;
                 var newTeam = '<input type="text" id="team'+newTeamNum+'" value="Team '+newTeamNum+'" class="teams"><br>';
                 $("#tourneySubmit").before(newTeam);
-                console.log(newTeam);
             }
         }
         else if (domCount > teamCount) {
